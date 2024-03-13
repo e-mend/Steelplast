041463673dcb1f60fb0bb2f2f8b5ca7e6871743d
Gabriel Paiva <paiva.gabriel911@gmail.com>
2024-03-13 18:12:16 -0300

    testing routing

--- index.php
+++ index.php
@@ -1,15 +1,17 @@
 <?php
 
 $routes = [
-    '/' => 'View/home.php',
-    '/about' => 'about.php',
-    '/contact' => 'contact.php',
+    '/' => 'home.php',
+    '/metal/bronze' => 'bronze.php',
 ];
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$url = str_replace('/GIT/Steelplast', '', $url);
+
+var_dump($url);
 
 if (array_key_exists($url, $routes)) {
-    include $routes[$url];
+    include "View/".$routes[$url];
 } else {
     http_response_code(404);
     echo '404 Not Found';
